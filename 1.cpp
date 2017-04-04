97ea092d4fa824ae53db3eb751d5cdb8dcdf1a2e
onufreivoksana <reivoxana@gmail.com>
2017-04-04 19:25:49 +0300

    add in

---
+++ 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+     int *A, *B;
+	  
+     *A = 10;
+     *B = *a + *b;
+     cîut << "b is" << *b << endl;
+
+     delete b;
+     delete a;
+
+     return 0;
+}
+
